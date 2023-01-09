57ebf8f355f0c8e0627c5e4dd6cb23f5bb07c322
beishenzhu <18376695585@163.com>
2023-01-09 15:14:09 +0800

    03_(掌握)项目-Logo的svg图片的两种使用方式

--- src/components/app-header/c-cpns/header-left/index.jsx
+++ src/components/app-header/c-cpns/header-left/index.jsx
@@ -1,9 +1,12 @@
+import IconLogo from '@/assets/svg/icon_logo'
 import React, { memo } from 'react'
 import { LeftWrapper } from './style'
 
 const HeaderLeft = memo(() => {
 	return (
-		<LeftWrapper>LeftWrapper</LeftWrapper>
+		<LeftWrapper>
+			<IconLogo />
+		</LeftWrapper>
 	)
 })
 
