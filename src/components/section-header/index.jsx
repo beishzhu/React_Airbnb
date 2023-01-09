d4cc678fc2c220cdc268508c963ee479ca741814
beishenzhu <18376695585@163.com>
2023-01-09 23:58:25 +0800

    12_(掌握)项目-首页-区域的header封装过程

---
+++ src/components/section-header/index.jsx
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types'
+import React, { memo } from 'react'
+import { HeaderWrapper } from './style'
+
+const SectionHeader = memo((props) => {
+	const {title,subtitle= "默认子标题"} = props
+	
+	
+	return (
+		<HeaderWrapper>
+			<h2 className='title'>{title}</h2>
+			<div className='subtitle'>{subtitle}</div>
+		</HeaderWrapper>
+	)
+})
+
+// rncp生成的模板 数据类型验证
+SectionHeader.propTypes = {
+	title:PropTypes.string,
+	subtitle:PropTypes.string
+}
+
+export default SectionHeader
--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -6,20 +6,21 @@ import { fetchHomeDataAction } from '@/store/moudles/home'
 import HomeBanner from './c-cpns/home-banner'
 // import hyRequest from '@/services'
 import {HomeWrapper} from './style'
+import SectionHeader from '@/components/section-header'
 
  const Home = memo(()=> {
 	
-	// 从 redux中获取数据
-	const {goodPriceInfo}  = useSelector((state)=>({
-		goodPriceInfo:state.home.goodPriceInfo
-	}),shallowEqual)  //优化的东西 shallowEqual：当发现改变的时候 才需要重新获取数据，重新渲染
+		// 发起网络请求获取数据
+		const dispath = useDispatch()
+		useEffect(()=>{
+			dispath(fetchHomeDataAction("xxx"))
+		},[dispath])
 	
-	
-	// 发起网络请求获取数据
-	const dispath = useDispatch()
-	useEffect(()=>{
-		dispath(fetchHomeDataAction("xxx"))
-	},[dispath])
+		// 从 redux中获取数据
+		const {goodPriceInfo}  = useSelector((state)=>({
+			goodPriceInfo:state.home.goodPriceInfo
+		}),shallowEqual)  //优化的东西 shallowEqual：当发现改变的时候 才需要重新获取数据，重新渲染
+		
 	
 	
 	
@@ -27,12 +28,15 @@ import {HomeWrapper} from './style'
 		<HomeWrapper>
 			<HomeBanner/>
 			<div className='content'>
-				<h2>{goodPriceInfo.title}</h2>
-				<ul>
-					{goodPriceInfo.list.map(item=>{
-						return <li key={item.id}>{item.name}</li>
-					})}
-				</ul>
+				<div className='good-price'>
+					<SectionHeader title={goodPriceInfo.title}/>
+					{/* <ul>
+						{goodPriceInfo.list.map(item => {
+							return <li key={item.id}>{item.name}</li>
+						})
+						}
+					</ul> */}
+				</div>
 			</div>
 		</HomeWrapper>
 	)
