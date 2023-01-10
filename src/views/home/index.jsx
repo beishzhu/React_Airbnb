fb2ef065cf685556e882be4207d21cf8836a5c0e
beishenzhu <18376695585@163.com>
2023-01-10 20:43:03 +0800

    15_(掌握)项目-集成UI组件库-Antd和MUI4.0版本

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,6 +1,9 @@
 import React, { memo, useEffect } from 'react'
 import { shallowEqual, useDispatch, useSelector } from 'react-redux'
+// import { Button, Space } from 'antd';
 
+// import Button from '@mui/material/Button'; 用不了
+import Button from '@material-ui/core/Button';  // 只能用 4.0版本
 
 import { fetchHomeDataAction } from '@/store/moudles/home'
 import HomeBanner from './c-cpns/home-banner'
@@ -27,6 +30,7 @@ import RoomItem from '@/components/room-item'
 	
 	return (
 		<HomeWrapper>
+		
 			<HomeBanner/>
 			<div className='content'>
 				<div className='good-price'>
@@ -40,6 +44,7 @@ import RoomItem from '@/components/room-item'
 					</ul>
 				</div>
 			</div>
+
 			<ul>
 				<li>ddddd</li>
 				<li>ddddd</li>
@@ -52,6 +57,19 @@ import RoomItem from '@/components/room-item'
 				<li>ddddd</li>
 				<li>ddddd</li>
 			</ul>
+      <Button variant="contained">Default</Button>
+      <Button variant="contained" color="primary">
+        Primary
+      </Button>
+      <Button variant="contained" color="secondary">
+        Secondary
+      </Button>
+      <Button variant="contained" disabled>
+        Disabled
+      </Button>
+      <Button variant="contained" color="primary" href="#contained-buttons">
+        Link
+      </Button>
 		</HomeWrapper>
 	)
 })
