ba40509544b354ad3292f9b3001082104f8b6082
beishenzhu <18376695585@163.com>
2023-01-12 19:04:32 +0800

    12_(掌握)项目-首页-ScrollView左侧按钮和按钮

--- src/base-ui/scroll-view/index.jsx
+++ src/base-ui/scroll-view/index.jsx
@@ -1,16 +1,18 @@
-import PropTypes from 'prop-types'
+import IconArrowLeft from '@/assets/svg/icon-arrow-left'
+import IconArrowRight from '@/assets/svg/icon-arrow-right'
 import React, { memo, useEffect, useRef, useState } from 'react'
 import { ViewWrapper } from './style'
 
 const ScrollView = memo((props) => {
-	// 定义内部的状态 是否显示右边按钮
+	// 定义内部的状态 是否显示按钮
+	const [showLeft,setShowLeft] = useState(false)
 	const [showRight,setShowRight] = useState(false)
 	const totalDistanceRef = useRef() // 记录totalDistance 的距离
 	
 	const [posIndex,setPosIndex] = useState(0)
 	// 组件渲染完毕 判断是否显示右侧的按钮
 	const scrollContentRef = useRef()
-	// useEffect 如果数据没有发生变化 是不会重新执行的，所以要传入第个参数来控制
+	// useEffect 如果数据没有发生变化 是不会重新执行的，所以要传入第个参数来控制 [props.children]
 	useEffect(()=>{
 	const scrollWidth =	scrollContentRef.current.scrollWidth  // 一共可以滚动的宽度
 	const clientWidth = scrollContentRef.current.clientWidth  // 内容本身的宽度
@@ -20,23 +22,36 @@ const ScrollView = memo((props) => {
 	},[props.children])
 	
 	// 事件处理逻辑
-	function rightClickHandle() {
-		const newIndex = posIndex + 1 // 记录每移动一个就索引+1
+	function controClickHandle(isRight) {
+		const newIndex = isRight ? posIndex + 1 : posIndex - 1 // 记录每移动一个就索引 -1
 		const newEl = scrollContentRef.current.children[newIndex] // 获取到是第几个item
 		const nweOffsetLeft = newEl.offsetLeft // 获取到 滚动距离 （注意相对定位的元素，所以要设置定位）
 		scrollContentRef.current.style.transform = `translate(-${nweOffsetLeft}px)`
 		setPosIndex(newIndex)
-		// 是否继续显示右边按钮
+		// 是否继续显示左边按钮
 		setShowRight(totalDistanceRef.current > nweOffsetLeft)
-	} 
+		setShowLeft(nweOffsetLeft > 0)
+		
+		
+	}
 	return (
 		<ViewWrapper>
-			<button>左边按钮</button>
-			{showRight && <button onClick={rightClickHandle}>右边按钮</button>}
+			{showLeft && (
+				 <div className='control left' onClick={e => controClickHandle(false)}>
+					<IconArrowLeft />
+				 </div>
+			)}
+			{showRight && (
+				<div className='control right' onClick={e => controClickHandle(true)}>
+					<IconArrowRight />
+				</div>
+			)}
 			{/* 插槽使用 */}
+		<div className='scroll'>
 			<div className='scroll-ceotent' ref={scrollContentRef}>
-				{props.children}
-			</div>
+					{props.children}
+				</div>
+		</div>
 		</ViewWrapper>
 	)
 })
