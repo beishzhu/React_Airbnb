e404ec3df0fc6217b716e60e15c1eb9f097aa4de
beishenzhu <18376695585@163.com>
2023-01-14 01:29:21 +0800

    05_(掌握)项目-全部-页码的组件展示和展示数据算法

--- src/views/entire/c-cpns/entire-pagination/index.jsx
+++ src/views/entire/c-cpns/entire-pagination/index.jsx
@@ -1,9 +1,33 @@
 import React, { memo } from 'react'
+import Pagination from '@material-ui/lab/Pagination';
+
 import { PaginationWrapper } from './style'
+import { useSelector } from 'react-redux';
 
 const EntirePagination = memo(() => {
+	
+	const {totalCount, currentPage, roomList} = useSelector((state)=>({
+		totalCount:state.entire.totalCount,
+		currentPage:state.entire.currentPage,
+		roomList:state.entire.roomList
+	}))
+	const startCount = currentPage * 20 + 1
+	const endCount = (currentPage + 1) * 20
+	
 	return (
-		<PaginationWrapper>EntirePagination</PaginationWrapper>
+		<PaginationWrapper>
+			{
+				!!roomList.length && (
+				<div className='info'>
+					<Pagination count={totalCount} />
+					<div className='desc'>
+						第 {startCount} - {endCount} 个房源，共超过{totalCount}个
+					</div>
+				</div>
+				)
+			}
+				
+		</PaginationWrapper>
 	)
 })
 
