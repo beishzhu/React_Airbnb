59ef7c5bc6a12fcf297435b8210edcb9f59d7fb7
beishenzhu <18376695585@163.com>
2023-01-14 19:52:05 +0800

    09_(掌握)项目-全部-RoomItem的轮播图实现效果

--- src/components/room-item/index.jsx
+++ src/components/room-item/index.jsx
@@ -1,21 +1,51 @@
+import IconArrowLeft from '@/assets/svg/icon-arrow-left';
+import IconArrowRight from '@/assets/svg/icon-arrow-right';
 import Rating from '@material-ui/lab/Rating'; // 只能用 4.0版本
+import { Carousel } from 'antd';
+
 // import { Rating } from '@mui/material'
 import PropTypes from 'prop-types'
-import React, { memo } from 'react'
+import React, { memo, useRef } from 'react'
 import {ItemWrapper} from './style'
 
 
 
 const RoomItem = memo((props) => {
 	const {itemData,itemWidth="25%"} = props
+	const sliderRef = useRef()
+	function controlClickHandle(isRight = true) {
+		isRight ? sliderRef.current.next() : sliderRef.current.prev()
+	}
 	return (
 		<ItemWrapper 
 			verifyColor={itemData?.verify_info.text_colo || "#39576a"}
 			itemWidth={itemWidth}
 		>
 			<div className='inner'>
-				<div className='cover'>
+						{/* <div className='cover'>
 					<img src={itemData.picture_url} alt="" />
+				</div> */}
+				{/* 轮播图 */}
+				<div className='slider'>
+					<div className='control'>
+						<div className='btn left' onClick={e=>controlClickHandle(false)}>
+							<IconArrowLeft width='30' height='30'/>
+						</div>
+						<div className='btn right' onClick={e=>controlClickHandle(true)}>
+							<IconArrowRight width='30' height='30'/>
+						</div>
+					</div>
+					<Carousel dots={false} ref={sliderRef}>
+						{
+							itemData?.picture_urls?.map(item =>{
+								return (
+									<div className='cover' key="item">
+										<img src={item} alt="" />
+									</div>
+								)
+							})
+						}
+					</Carousel>
 				</div>
 				<div className='desc'>
 					{itemData.verify_info.messages.join(' · ')}
