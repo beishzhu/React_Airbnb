3cdc7a72d429c5174105cdc3e2c590e0e97d6143
beishenzhu <18376695585@163.com>
2023-01-19 00:13:44 +0800

    08_(掌握)项目-页面切换滚动到顶部

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React, { memo } from 'react'
 import { useRoutes } from 'react-router-dom'
 import AppFooter from './components/app-footer'
 import AppHeader from './components/app-header'
+import useScrollTop from './hooks/useScrollTop'
 import routes from './router'
 
 
 export default memo(function App() {
+	useScrollTop()
 	return (
 		<div className='app'>
 		  {/* <AppHeader></AppHeader> */}
